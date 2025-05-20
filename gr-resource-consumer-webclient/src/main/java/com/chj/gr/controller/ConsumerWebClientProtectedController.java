f5a97c971326a52a603e43109157e66ace2d1f6c
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-20 15:34:31 +0200

    setup swagger for webclient consumer

--- gr-resource-consumer-webclient/src/main/java/com/chj/gr/controller/ConsumerWebClientProtectedController.java
+++ gr-resource-consumer-webclient/src/main/java/com/chj/gr/controller/ConsumerWebClientProtectedController.java
@@ -9,14 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
 public class ConsumerWebClientProtectedController {
 
 	@Autowired
 	private WebClient webClient;
 
+	@Operation(summary = "Call MS1 to access MS2", security = @SecurityRequirement(name = "oauth2"))
     @GetMapping("/call-ms1-protected")
-    public String callMs1(@RegisteredOAuth2AuthorizedClient("consumer-webclient") OAuth2AuthorizedClient authorizedClient) {
+    public String callMs1(@Parameter(hidden = true) @RegisteredOAuth2AuthorizedClient("consumer-webclient") OAuth2AuthorizedClient authorizedClient) {
     	String responseJson =  this.webClient
                 .get()
                 .uri("http://GR-API-GATEWAY/ms1/gr-ms1-resource/protected/ms2")
@@ -38,8 +43,9 @@ public class ConsumerWebClientProtectedController {
         return "GR-RESOURCE-CONSUMER-WEBCLIENT ==> GR-API-GATEWAY ==> " + responseJson;
     }
     
+	@Operation(summary = "Call MS2", security = @SecurityRequirement(name = "oauth2"))
     @GetMapping("/call-ms2-protected")
-    public String callMs2(@RegisteredOAuth2AuthorizedClient("consumer-webclient") OAuth2AuthorizedClient authorizedClient) {
+    public String callMs2(@Parameter(hidden = true) @RegisteredOAuth2AuthorizedClient("consumer-webclient") OAuth2AuthorizedClient authorizedClient) {
     	String responseJson =  this.webClient
                 .get()
                 .uri("http://GR-API-GATEWAY/ms2/gr-ms2-resource/protected/get")
