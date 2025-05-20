8d25490ba5bcdee6148a126591b9aa32392cb0b9
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-20 15:43:12 +0200

    setup swagger for resttemplate consumer

--- gr-resource-consumer-resttemplate/src/main/java/com/chj/gr/controller/ConsumerRestTemplateProtectedController.java
+++ gr-resource-consumer-resttemplate/src/main/java/com/chj/gr/controller/ConsumerRestTemplateProtectedController.java
@@ -11,14 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
 public class ConsumerRestTemplateProtectedController {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Operation(summary = "Call MS1 to access MS2", security = @SecurityRequirement(name = "oauth2"))
     @GetMapping("/call-ms1-protected")
-    public String callMs1(@RegisteredOAuth2AuthorizedClient("consumer-resttemplate") OAuth2AuthorizedClient authorizedClient) {
+    public String callMs1(@Parameter(hidden = true) @RegisteredOAuth2AuthorizedClient("consumer-resttemplate") OAuth2AuthorizedClient authorizedClient) {
     	
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(authorizedClient.getAccessToken().getTokenValue());
@@ -33,8 +38,9 @@ public class ConsumerRestTemplateProtectedController {
         return "GR-RESOURCE-CONSUMER-RESTTEMPLATE ==> GR-API-GATEWAY ==> " + response.getBody();
     }
     
+    @Operation(summary = "Call MS2", security = @SecurityRequirement(name = "oauth2"))
     @GetMapping("/call-ms2-protected")
-    public String callMs2(@RegisteredOAuth2AuthorizedClient("consumer-resttemplate") OAuth2AuthorizedClient authorizedClient) {
+    public String callMs2(@Parameter(hidden = true) @RegisteredOAuth2AuthorizedClient("consumer-resttemplate") OAuth2AuthorizedClient authorizedClient) {
     	
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(authorizedClient.getAccessToken().getTokenValue());
--- gr-resource-consumer-resttemplate/src/main/java/com/chj/gr/controller/ConsumerRestTemplatePublicController.java
+++ gr-resource-consumer-resttemplate/src/main/java/com/chj/gr/controller/ConsumerRestTemplatePublicController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class ConsumerRestTemplatePublicController {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Operation(summary = "Call MS1 public endpoint")
     @GetMapping("/call-ms1-public")
     public String callMs1Public() {
     	
@@ -18,8 +21,7 @@ public class ConsumerRestTemplatePublicController {
         return "GR-RESOURCE-CONSUMER-RESTTEMPLATE ==> GR-API-GATEWAY ==> " + restTemplate.getForObject(url, String.class);
     }
     
-
-
+    @Operation(summary = "Call MS2 public endpoint")
     @GetMapping("/call-ms2-public")
     public String callMs4Public() {
     	
